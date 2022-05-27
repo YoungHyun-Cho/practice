2f285b4a00d8ec31b9ed3fdd10230e09594b44ef
0hyun.cho <psyhyun1030@gmail.com>
2022-05-27 12:55:38 +0900

    5장까지 완료

--- src/main/java/com/codestates/springboot/web/IndexController.java
+++ src/main/java/com/codestates/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.codestates.springboot.web;
 
+import com.codestates.springboot.config.auth.LoginUser;
 import com.codestates.springboot.config.auth.dto.SessionUser;
 import com.codestates.springboot.service.posts.PostsService;
 import com.codestates.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) model.addAttribute("userName", user.getName());
         return "index";
     }
